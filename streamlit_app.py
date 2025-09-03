8ff56d4855759e7246f4fa4d50fdedbb298d7d35
SistGoy <45516559+SistGoyPeru@users.noreply.github.com>
2025-09-03 12:21:52 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
-from clases import liga
+import polars as pl
+
+class liga():
+    def __init__(self, archivo):
+        self.archivo = archivo
+        self.df = pl.read_csv(archivo)
+
+    def data(self):
+        return self.df
 
 
 
