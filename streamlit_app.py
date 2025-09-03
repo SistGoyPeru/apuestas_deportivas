d9e7c487d5769f9c097920497cffcdd31e1ba5dd
SistGoy <45516559+SistGoyPeru@users.noreply.github.com>
2025-09-03 11:44:17 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit as st
-from scraper import FootballDataScraper
+
 
 
 def main():
-  st.write(FootballDataScraper("https://www.livefutbol.com/todos_partidos/per-primera-division-2025-clausura/"))
-
+  st.write("alex")
 
 if __name__ == "__main__":
   main()
