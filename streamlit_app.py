72f82cac0cf22d6dfc3a30bba911d53c3e24bd14
SistGoy <45516559+SistGoyPeru@users.noreply.github.com>
2025-09-04 23:52:45 +0000

    fecha

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 from clases import liga
+from datetime import datetime,date
 
 def main():
   df_total=liga("data_ligas.csv")
-  st.set_page_config(layout="wide", page_title="Pronósticos de Ligas del Mundo - SistGoy")
+  st.set_page_config(page_title="Pronósticos de Ligas del Mundo - SistGoy")
 
-  st.title("Dashboard de Pronósticos Deportivos ⚽")
+  st.title("Pronósticos Deportivos ⚽")
+  st.markdown("---")
+  fecha=st.date_input("Selecciona la Fecha para Pronosticar",date.today(),format="DD.MM.YYYY",width=250 )
   
   
   
