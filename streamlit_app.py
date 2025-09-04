2f3a7ec2a69d44908c9be11e649015ed1fd04c60
SistGoy <45516559+SistGoyPeru@users.noreply.github.com>
2025-09-04 23:40:34 +0000

    nuebvo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ from clases import liga
 
 def main():
   df_total=liga("data_ligas.csv")
-  st.write(df_total.data())
+  st.set_page_config(layout="wide", page_title="Pronósticos de Ligas del Mundo - SistGoy")
+
+  st.title("Dashboard de Pronósticos Deportivos ⚽")
+  
   
   
 
