f9a2760f009419b97a17728c53e4e0e907f83dbf
SistGoy <45516559+SistGoyPeru@users.noreply.github.com>
2025-09-05 00:32:26 +0000

    cambios

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,20 @@ import polars as pl
 from datetime import datetime,date
 
 def main():
+  
+  colores = {
+    "Local": "#28a745",     # Verde para victoria local
+    "Empate": "#ffc107",    # Amarillo para empate
+    "Visitante": "#dc3545"  # Rojo para victoria visitante
+    }
+
+    
+
+
+
+
+
+
   df_total=liga("data_ligas.csv")
   df=df_total.data()
   st.set_page_config(page_title="Pronósticos de Ligas del Mundo - SistGoy")
@@ -29,9 +43,39 @@ def main():
         selected_row = event.selection['rows'][0]
         local = df_final[selected_row, 'Local']
         visita = df_final[selected_row, 'Visita']
+
+
+        st.markdown(f"### Pronóstico para: {local} vs {visita} ({LigasDisponibles})")
+        tab1, tab2, tab3 = st.tabs(["📊 Resumen", "📋 Detalle de Pronósticos", "🎯 Precisión del Modelo"])
+        with tab1:
+            with st.expander("Probabilidades de Resultado ( 1-X-2 ) ⚽:",expanded=True):
+                prob_local = df_total.VictoriaLocal(LigasDisponibles,local, visita)*100
+                prob_empate = df_total.EmpateResultado(LigasDisponibles,local, visita)*100
+                prob_visitante = df_total.VictoriaVisita(LigasDisponibles,local, visita)*100
+
+                cuota_local = 100 / prob_local if prob_local > 0 else 0
+                cuota_empate = 100 / prob_empate if prob_empate > 0 else 0
+                cuota_visitante = 100 / prob_visitante if prob_visitante > 0 else 0
+
+                col1, col2, col3 = st.columns(3)
+
+                with col1:
+                    st.markdown(f"""
+                    <div style="background-color: #f0f2f6; padding: 15px; border-radius: 5px; text-align: center; border-left: 5px solid {colores['Local']};">
+                        <h3 style="margin: 0; font-size: 1.2rem; color: #555;">Victoria Local</h3>
+                        <p style="font-size: 2.5rem; font-weight: bold; color: {colores['Local']}; margin: 5px 0 0 0;">
+                            {prob_local:.1f}%
+                        </p>
+                        <p style="font-size: 1.2rem; margin: 0; color: #777;">
+                            Cuota: <span style="font-weight: bold;">{cuota_local:.2f}</span>
+                        </p>
+                    </div>
+                    """, unsafe_allow_html=True)
+            
+
         
         
-        st.badge(local+" vs "+visita,color="gray") 
+        
   
   
   
