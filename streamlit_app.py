c00f19c9143026216330f7dd478b772573c94cba
SistGoy <45516559+SistGoyPeru@users.noreply.github.com>
2025-09-05 00:05:20 +0000

    ligas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,38 @@
 import streamlit as st
 from clases import liga
+import polars as pl
 from datetime import datetime,date
 
 def main():
   df_total=liga("data_ligas.csv")
+  df=df_total.data()
   st.set_page_config(page_title="Pronósticos de Ligas del Mundo - SistGoy")
 
   st.title("Pronósticos Deportivos ⚽")
   st.markdown("---")
   fecha=st.date_input("Selecciona la Fecha para Pronosticar",date.today(),format="DD.MM.YYYY",width=250 )
+  dfecha=df.filter(pl.col("Fecha")==fecha.strftime("%d.%m.%Y")).sort("Liga")
+
+  
+  ld=dfecha['Liga'].unique().sort() 
+  LigasDisponibles=st.selectbox("Liga Para Pronosticar",ld) 
+  df_final=dfecha.filter(pl.col("Liga")==LigasDisponibles)
+
+  event=st.dataframe(
+        df_final,
+        on_select='rerun',
+        selection_mode='single-row'
+
+    )
+  
+  if len(event.selection['rows']):
+        selected_row = event.selection['rows'][0]
+        local = df_final[selected_row, 'Local']
+        visita = df_final[selected_row, 'Visita']
+        
+        
+        st.badge(local+" vs "+visita,color="gray") 
+  
   
   
   
