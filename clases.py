f2f400e4d8058650e063d9d2843a2fb2e664b3c6
SistGoy <45516559+SistGoyPeru@users.noreply.github.com>
2025-09-22 11:39:10 +0000

    ass

--- clases.py
+++ clases.py
@@ -861,11 +861,17 @@ class liga():
             "12 Local o Visita y Más de 1.5 Goles": self.masde15goles(ligas, local, visita)*(self.VictoriaLocal(ligas, local, visita) +self.VictoriaVisita(ligas, local, visita)),
             "1X Local o Empate y Más de 2.5 Goles":self.masde25goles(ligas, local, visita)*(self.VictoriaLocal(ligas, local, visita) +self.EmpateResultado(ligas, local, visita)),
             "2X Visita o Empate y Más de 2.5 Goles":self.masde25goles(ligas, local, visita)*(self.VictoriaVisita(ligas, local, visita) +self.EmpateResultado(ligas, local, visita)),
-            "12 Local o Visita y Más de 2.5 Goles":self.masde25goles(ligas, local,visita)*(self.VictoriaLocal(ligas, local, visita) +self.VictoriaVisita(ligas, local, visita))    
+            "12 Local o Visita y Más de 2.5 Goles":self.masde25goles(ligas, local,visita)*(self.VictoriaLocal(ligas, local, visita) +self.VictoriaVisita(ligas, local, visita)) ,
+            "1X Local o Empate y Ambos Marcan":self.ambosmarcan(ligas, local, visita)*(self.VictoriaLocal(ligas, local, visita) +self.EmpateResultado(ligas, local, visita)) ,
+            "2X Visita o Empate y Ambos Marcan":self.ambosmarcan(liga, local, visita)*(self.VictoriaVisita(ligas, local, visita) +self.EmpateResultado(ligas, local, visita)) ,
+            "12 Local o Visita y Ambos Marcan":self.ambosmarcan(ligas, local,visita)*(self.VictoriaLocal(ligas, local, visita) +self.VictoriaVisita(ligas, local, visita)),
+            "1X Local o Empate y Solo Uno Marca":self.solounomarca(ligas,local,visita)*(self.VictoriaLocal(ligas, local, visita) +self.EmpateResultado(ligas, local, visita)),
+            "2X Visita o Empate y Solo Uno Marca":self.solounomarca(ligas,local,visita)*(self.VictoriaVisita(ligas, local, visita) +self.EmpateResultado(ligas, local, visita)),
+            "12 Local o Visita y Solo Uno Marca":self.solounomarca(ligas,local,visita)*(self.VictoriaLocal(local, visita) +self.VictoriaVisita(ligas, local, visita))
             
         }
         df_resultados = pl.DataFrame({
-            "Tipo de Apuesta Combinada": list(resultados.keys()),
+            "Tipo de Apuesta Combinada": list(resultados.keys())
             "Probabilidad": [f"{value:.2%}" for value in resultados.values()],
             "Cuota Sugerida (Decimal)": [f"{(1/value):.2f}" if value > 0 else "N/A" for value in resultados.values()]
         })      
