8c3d43e91c98c1350f0d68871ff388ffe892487d
SistGoy <45516559+SistGoyPeru@users.noreply.github.com>
2025-09-24 12:58:35 +0000

    as

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ def main():
                     st.subheader(local)
 
                     Elocal = pl.DataFrame({
-                        "Descripcion": [
+                        "Local": [
                             "PPP General",
                             "PPP Local",
                             "% Victoria Local",
@@ -90,7 +90,7 @@ def main():
 
                         ],
 
-                        "Local": [[format(df_total.PPP(liga_E,local), '.2f')],
+                        "Valor": [[format(df_total.PPP(liga_E,local), '.2f')],
                                   [format(df_total.PPP_local(liga_E, local), '.2f')],
                                   [format(df_total.TotalVictoriasEquipoLocal(liga_E, local)/df_total.TotalDisputadosEquipoLocal(liga_E, local)*100, '.2f')+"%"if df_total.TotalDisputadosEquipoLocal(liga_E, local) > 0 else "0.00%"],
                                   [format(df_total.TotalEmpatesEquipoLocal(liga_E, local)/df_total.TotalDisputadosEquipoLocal(
