13ad6b09d1efd258594f139d50eb2db6d794b4b4
SistGoy <45516559+SistGoyPeru@users.noreply.github.com>
2025-10-18 13:22:12 +0200

    Refactor data filtering for local and visiting teams
    Updated data filtering to use df_total instead of df for local and visiting team data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ def main():
 
     df_total = liga("data_ligas.csv")
     df = df_total.data()
+    df=df.filter(pl.col('GA').is_null())
     
    
  
@@ -86,7 +87,7 @@ def main():
                 
                 with col1:
                     st.subheader(local)
-                    d=df.filter(
+                    d=df_total.filter(
                         (pl.col("Local")==local )| (pl.col("Visita")==local),
                         pl.col("Liga")==ligas,
                         )
@@ -96,7 +97,7 @@ def main():
                     
                 with col2:
                     st.subheader(visita) 
-                    d=df.filter(
+                    d=df_total.filter(
                         (pl.col("Local")==visita )| (pl.col("Visita")==visita),
                         pl.col("Liga")==ligas,
                         )
