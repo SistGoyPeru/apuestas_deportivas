84246f837df469f66c186a9458f81cefb91a9eee
SistGoy <45516559+SistGoyPeru@users.noreply.github.com>
2025-10-18 13:24:47 +0200

    Refactor dataframe filtering to use df1
    Updated dataframe variable to use df1 for filtering.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ def main():
 
     df_total = liga("data_ligas.csv")
     df = df_total.data()
+    df1 = df_total.data()
     df=df.filter(pl.col('GA').is_null())
     
    
@@ -87,7 +88,7 @@ def main():
                 
                 with col1:
                     st.subheader(local)
-                    d=df_total.filter(
+                    d=df1.filter(
                         (pl.col("Local")==local )| (pl.col("Visita")==local),
                         pl.col("Liga")==ligas,
                         )
@@ -97,7 +98,7 @@ def main():
                     
                 with col2:
                     st.subheader(visita) 
-                    d=df_total.filter(
+                    d=df1.filter(
                         (pl.col("Local")==visita )| (pl.col("Visita")==visita),
                         pl.col("Liga")==ligas,
                         )
